b00f94d9c038808eaa208b276e6cc6d87c0011bd
EdictProg <edik.tigranyan@yandex.ru>
2023-12-06 22:57:07 +0300

    Interface update 1.1

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,52 +1,51 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-GainAudioProcessorEditor::GainAudioProcessorEditor (GainAudioProcessor& p) : AudioProcessorEditor (&p), audioProcessor (p)
+GainAudioProcessorEditor::GainAudioProcessorEditor(GainAudioProcessor& p) : AudioProcessorEditor(&p), audioProcessor(p)
 {
-    setSize (200, 400);
-    
+    setSize(200, 400);
+
     addAndMakeVisible(SlBar);
     SlBar.setSliderStyle(juce::Slider::SliderStyle::LinearBarVertical);
 
-    SlBar.setRange(-60.0, 60.0, 0.1);  
+    SlBar.setRange(-60.0, 60.0, 0.1);
     SlBar.setColour(juce::Slider::trackColourId, juce::Colours::pink.darker(0.2));
     SlBar.setAlpha(1.0);
-    
-    SlBar.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 160, SlBar.getTextBoxHeight());
+
+    SlBar.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 160, SlBar.getTextBoxHeight());
     SlBar.setTextValueSuffix(" Db");
 
     SlBar.addListener(this);
 
     setResizable(true, true);
-    setResizeLimits(200, 150, 2000, 2000);
+    setResizeLimits(100, 75, 2000, 2000);
 }
 
 GainAudioProcessorEditor::~GainAudioProcessorEditor()
 {
-
 }
 
 
-void GainAudioProcessorEditor::paint (juce::Graphics& g)
+void GainAudioProcessorEditor::paint(juce::Graphics& g)
 {
-    g.fillAll (juce::Colours::black.brighter(0.1));
+    g.fillAll(juce::Colours::black.brighter(0.1));
 
-    g.setColour (juce::Colours::pink);
-    g.setFont (17.0f);
-    g.drawFittedText ("Gain Control", getLocalBounds(), juce::Justification::topRight, 1);
+    g.setColour(juce::Colours::pink.darker(0.2));
+    g.setFont(getWidth() / 11);
+    g.drawFittedText("Gain Control", getLocalBounds(), juce::Justification::topRight, 1);
     g.drawVerticalLine(getWidth() / 2, 0, getHeight());
     g.drawVerticalLine(0, 0, getHeight());
 }
 
 void GainAudioProcessorEditor::resized()
 {
-    SlBar.setBounds(0,0,getWidth()/2, getHeight());
+    SlBar.setBounds(0, 0, getWidth() / 2, getHeight());
 }
 
-void GainAudioProcessorEditor::sliderValueChanged(juce::Slider* slider) 
+void GainAudioProcessorEditor::sliderValueChanged(juce::Slider* slider)
 {
-    if (slider == &SlBar) 
+    if (slider == &SlBar)
     {
-        audioProcessor.Change = pow(10, SlBar.getValue()/20);
+        audioProcessor.Change = pow(10, SlBar.getValue() / 20);
     }
 }
