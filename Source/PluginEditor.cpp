97a1282f720f4fe27b363b773e69ff2a5c8970d5
EdictProg <edik.tigranyan@yandex.ru>
2023-12-06 22:32:31 +0300

    Interface update

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -3,32 +3,44 @@
 
 GainAudioProcessorEditor::GainAudioProcessorEditor (GainAudioProcessor& p) : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    setSize (200, 200);
+    setSize (200, 400);
     
     addAndMakeVisible(SlBar);
     SlBar.setSliderStyle(juce::Slider::SliderStyle::LinearBarVertical);
-    SlBar.setRange(-60.0, 60.0);
+
+    SlBar.setRange(-60.0, 60.0, 0.1);  
+    SlBar.setColour(juce::Slider::trackColourId, juce::Colours::pink.darker(0.2));
+    SlBar.setAlpha(1.0);
+    
+    SlBar.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 160, SlBar.getTextBoxHeight());
+    SlBar.setTextValueSuffix(" Db");
 
     SlBar.addListener(this);
+
+    setResizable(true, true);
+    setResizeLimits(200, 150, 2000, 2000);
 }
 
 GainAudioProcessorEditor::~GainAudioProcessorEditor()
 {
+
 }
 
 
 void GainAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    g.fillAll (juce::Colours::transparentWhite);
+    g.fillAll (juce::Colours::black.brighter(0.1));
 
-    //g.setColour (juce::Colours::pink);
-    //g.setFont (15.0f);
-    //g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.setColour (juce::Colours::pink);
+    g.setFont (17.0f);
+    g.drawFittedText ("Gain Control", getLocalBounds(), juce::Justification::topRight, 1);
+    g.drawVerticalLine(getWidth() / 2, 0, getHeight());
+    g.drawVerticalLine(0, 0, getHeight());
 }
 
 void GainAudioProcessorEditor::resized()
 {
-    SlBar.setBounds(getLocalBounds());
+    SlBar.setBounds(0,0,getWidth()/2, getHeight());
 }
 
 void GainAudioProcessorEditor::sliderValueChanged(juce::Slider* slider) 
